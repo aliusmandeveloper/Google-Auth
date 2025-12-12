5aaf454e23cada6ddd046b16c7cf53f6d35fb6df
Muhammad Ali Usman <aliusmanijaz143@example.com>
2025-12-12 22:30:53 +0500

    google url 2

--- auth-varification/frontend/src/app/components/GoogleWrapper.jsx
+++ auth-varification/frontend/src/app/components/GoogleWrapper.jsx
@@ -4,7 +4,12 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function GoogleWrapper({ children }) {
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider
+      clientId={
+        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ||
+        "183782225704-qrpnfu4ohu5gukc4s5f1c3eumv7kvk05.apps.googleusercontent.com"
+      }
+    >
       {children}
     </GoogleOAuthProvider>
   );
